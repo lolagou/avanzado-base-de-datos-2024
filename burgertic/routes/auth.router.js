967ec9ab531bb8ee0311133471464c3b152d957d
lolagou <147620896+lolagou@users.noreply.github.com>
2024-09-18 10:33:55 -0300

    rest

--- burgertic/routes/auth.router.js
+++ burgertic/routes/auth.router.js
@@ -4,9 +4,11 @@ import AuthController from "../controllers/auth.controller.js";
 const router = Router();
 
 // ------------- COMPLETAR LAS RUTAS DE LOGIN Y REGISTER -------------
-router.post("/", verifyToken, verifyAdmin, AuthController.createUsuario);
+router.post("/", AuthController.getUsuario);
 router.put("/:id", verifyToken, verifyAdmin, AuthController.getUsuarioById);
 router.delete("/:id", verifyToken, verifyAdmin, AuthController.getUsuarioByEmail);
 export default router;
+
+
 
 
